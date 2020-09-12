cf65e78c1d10d7f5e12a12a8ef6d5c553b3d4e12
Dmitry <dmitry.petrov0918@gmail.com>
2020-09-12 01:10:13 -0400

    Add Styled components library

--- src/components/Game/Cell.js
+++ src/components/Game/Cell.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlag, faBomb } from '@fortawesome/free-solid-svg-icons';
 
